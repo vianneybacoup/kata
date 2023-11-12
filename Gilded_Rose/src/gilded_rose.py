38c0742a297b55cfbe217fcaa7b71cf7dfe9f89f
Vianney Bacoup <vianney.bacoup@gmail.com>
2023-11-12 18:48:58 +0100

    [Gilded rose] Refactor nested ifs and extract methods

--- Gilded_Rose/src/gilded_rose.py
+++ Gilded_Rose/src/gilded_rose.py
@@ -7,33 +7,45 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+            if self.is_regular_item(item):
+                self.try_decreasing_quality(item)
+            elif item.name == "Aged Brie":
+                self.try_increasing_quality(item)
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.try_increasing_quality(item)
+                if item.sell_in < 11:
+                    self.try_increasing_quality(item)
+
+                if item.sell_in < 6:
+                    self.try_increasing_quality(item)
+
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
+
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                if self.is_regular_item(item):
+                    self.try_decreasing_quality(item)
+                elif item.name == "Aged Brie":
+                    self.try_increasing_quality(item)
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = 0
+
+    def try_decreasing_quality(self, item):
+        if self.has_quality(item):
+            item.quality = item.quality - 1
+
+    def has_quality(self, item):
+        return item.quality > 0
+
+    def try_increasing_quality(self, item):
+        if self.is_quality_not_too_high(item):
+            item.quality = item.quality + 1
+
+    def is_quality_not_too_high(self, item):
+        return item.quality < 50
+
+    def is_regular_item(self, item):
+        return item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros"
 
 
 class Item:
